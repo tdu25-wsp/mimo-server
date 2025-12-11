d178d7745425cf76258d0b6cce9a7aa95d8ee3c0
shutame <main@shuta.me>
2025-12-12 00:16:42 +0900

    fix env and axum routing

--- src/server.rs
+++ src/server.rs
@@ -41,7 +41,7 @@ pub async fn start_server(
     println!("Creating routes...");
     let app = Router::new()
         .nest("/api", create_api_routes(memo_service))
-        .nest("", create_share_routes())
+        .merge(create_share_routes())
         .layer(cors);
 
     let listener = TcpListener::bind(addr).await?;
