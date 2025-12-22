4ef77659af6ed5210f68bc1462321ebd19d6a56d
shutame <main@shuta.me>
2025-12-23 00:33:12 +0900

    fix tag service filename

--- src/services/mod.rs
+++ src/services/mod.rs
@@ -1,7 +1,7 @@
 mod memo_service;
 mod summary_service;
-pub mod tag_services;
+pub mod tag_service;
 
 pub use memo_service::MemoService;
 pub use summary_service::SummaryService;
-pub use tag_services::TagService;
+pub use tag_service::TagService;
